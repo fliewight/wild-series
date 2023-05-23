66c340c18d4198af214a27a186797c855fff20de
fliewight <lissjulien@gmail.com>
2023-05-23 14:43:55 +0200

    Ajout des routes complexes dynamiques

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-Class ProgramController extends AbstractController
+#[Route('/program', name: 'program_')]
+class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    // On utilise un pattern \d+ pour que l'ID soit obligatoirement un entier
+    #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }
