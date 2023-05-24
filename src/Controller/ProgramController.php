1fb8d9bf3c04de7c31436833ffe13bb66fe398d2
fliewight <lissjulien@gmail.com>
2023-05-24 20:16:06 +0200

    Added new bootstrap css and add routes for categories listing with verification if she's not empty

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,27 +2,37 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/program', name: 'program_')]
-class ProgramController extends AbstractController
+Class ProgramController extends AbstractController
 {
+
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+         $programs = $programRepository->findAll();
+
+         return $this->render('program/index.html.twig', [
+            'programs' => $programs,
         ]);
     }
 
     // On utilise un pattern \d+ pour que l'ID soit obligatoirement un entier
     #[Route('/{id}', methods: ['GET'], requirements: ['id'=>'\d+'], name: 'show')]
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
+        //$program = $programRepository->findOneBy(['id' => $id]);
+        // OU plus simple
+        //$program = $programRepository->findOneById($id);
+        // Si 1 seul argument
+        $program = $programRepository->find($id);
+
         return $this->render('program/show.html.twig', [
-            'id' => $id,
+            'program' => $program,
         ]);
     }
 }
